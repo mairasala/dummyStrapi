7c3919c51113e692efe88af17b78e4343cc9162c
mairasala <mairasala@gmail.com>
2020-03-27 21:45:07 +0100

    dummy projet

---
+++ api/something/controllers/something.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/controllers.html#core-controllers)
+ * to customize this controller
+ */
+
+module.exports = {
+  async test(ctx) {
+    return {
+      data: true
+    }
+  }
+};
